e98b12ffcee791b888c5a246682349e70819526c
mt02v <141305346+mt02v@users.noreply.github.com>
2023-09-09 01:38:01 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  
   def new
     @list = List.new
   end
@@ -8,6 +9,7 @@ class ListsController < ApplicationController
     list.save
     redirect_to list_path(list.id)
   end
+  
   def index
     @lists = List.all
   end
@@ -15,11 +17,21 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
-
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
   private
+  
   def list_params
     params.require(:list).permit(:title, :body)
   end
-
-
+  
 end
